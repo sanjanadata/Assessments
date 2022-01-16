b24a9b5231440bb1cf68671873a27b4e842210ae
sanjanadata <84971387+sanjanadata@users.noreply.github.com>
2022-01-16 18:33:25 +0530

    Create 1_string.java

---
+++ 1_string.java
@@ -0,0 +1,22 @@
+package self_learning_1;
+
+public class string {
+	  public static void main(String[] args) {
+	        String a = "5";
+	        System.out.println("String = "+a);
+
+	        int b = Integer.parseInt(a);
+	        System.out.println("Int = "+b);
+
+	        int c = Integer.parseUnsignedInt(a);
+	        System.out.println("Unsigned Int = "+c);
+
+	        float e = Float.parseFloat(a);
+	        System.out.println("Float = "+e);
+
+	        double f = Double.parseDouble(a);
+	        System.out.println("Double = "+f);
+
+	    }
+
+}
