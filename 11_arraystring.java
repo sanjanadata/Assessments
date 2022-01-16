d2f23c2b8fc1d41fe4ed4c0daf72388ac1697cf2
sanjanadata <84971387+sanjanadata@users.noreply.github.com>
2022-01-16 18:58:36 +0530

    Create 11_arraystring.java

---
+++ 11_arraystring.java
@@ -0,0 +1,22 @@
+package self_learning_11;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class array_string {
+	@SuppressWarnings("resource")
+	public static void main (String [] arg) throws IOException{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter The String");
+		String Path =sc.nextLine();
+		if (Path.length() <= 15 )
+	{
+		System.out.println("The size of the string Is under StringSize");
+		System.out.println("The Lenth Of String is : "+Path.length());
+		
+	}
+	else {
+		System.out.println("Out of String Size");	
+		System.out.println("The Lenth Of String is : "+Path.length());
+	}
+}
+}
