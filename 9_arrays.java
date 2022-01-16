edb5373b76cca795d93ec15066cc4f935a95ba45
sanjanadata <84971387+sanjanadata@users.noreply.github.com>
2022-01-16 18:56:19 +0530

    Create 9_arrays.java

---
+++ 9_arrays.java
@@ -0,0 +1,21 @@
+package self_learning_9;
+
+public class Arrays_D {
+
+	public static void main(String[] args) {
+
+		//single-dimensional array
+		int A[]= {1,2,3,4,5};
+		for(int i=0;i<5;i++) {
+		System.out.println("Elements of array A: "+A[i]);
+		}
+
+
+		//multidimensional array
+		int[][] B = {
+		            {2,4,6,6,8}, 
+		            {4,8,12} };
+		      
+		      System.out.println("\nLength of row 1: " + B[0].length);
+		      }
+		}
